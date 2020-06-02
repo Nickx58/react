504882b0ce5b73a38b3242d3e98e570442798761
Nickx58 <nikhilx58@gmail.com>
2020-06-02 23:11:17 +0530

    Event Handling Binding

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 
 class Message extends Component {
-  state = {
-    message: "Hello Visitor",
-  };
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello Visitor",
+    };
+    // this.changeText = this.changeText.bind(this);
+  }
 
   changeText = () => {
-    this.setState({ message: "Thanks for subscribing" });
+    this.setState({
+      message: "Thanks for coming UP!",
+    });
   };
 
   render() {
     return (
       <div>
         <h1> {this.state.message} </h1>
+        {/* <button onClick={this.changeText.bind(this)}> Subscribe </button> */}
+        {/* <button onClick={() => this.changeText()}> Subscribe </button> */}
         <button onClick={this.changeText}> Subscribe </button>
       </div>
     );
