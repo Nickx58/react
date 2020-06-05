49116168b731688c37674b018aa81e05202382da
Nickx58 <nikhilx58@gmail.com>
2020-06-05 20:05:02 +0530

    Hooks with prev State function

--- src/hooksComponents/Counter.js
+++ src/hooksComponents/Counter.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  let initialState = 0;
+  const [count, setCount] = useState(initialState);
+  const incrementByFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Count {count}</button>
+      <p>{count}</p>
+      <button onClick={() => setCount(initialState)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+      <button onClick={incrementByFive}>Increment By 5</button>
     </>
   );
 };
