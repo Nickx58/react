571911e4d2f31d784577753e65e4ee47110f8351
Nickx58 <nikhilx58@gmail.com>
2020-06-05 19:47:16 +0530

    Hooks Testing

---
+++ src/AppHooks.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AppHooks.css";
+
+function AppHooks() {
+  return (
+    <div className="AppHooks">
+      <h1> Hooks Testing </h1>
+    </div>
+  );
+}
+
+export default AppHooks;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-import Todo from "./components/Todo";
+// import Todo from "./components/Todo";
+import AppHooks from "./AppHooks";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Todo />
+    <AppHooks />
   </React.StrictMode>,
   document.getElementById("root")
 );
